9f9faf72b10117b0d006abd283d74c537ef1ed6c
Ham-135 <115642877+Ham-135@users.noreply.github.com>
2022-10-12 16:34:24 +0200

    Update 6872265039.lua

--- Modules/6872265039.lua
+++ Modules/6872265039.lua
@@ -1,33 +1,213 @@
-repeat task.wait() until game:IsLoaded()
-function betterisfile(path)
-    local suc, res = pcall(function() readfile(path) end)
-    return suc and res ~= nil
-end
-spawn(function()
-    local queueteleport = syn and syn.queue_on_teleport or queue_on_teleport or fluxus and fluxus.queue_on_teleport
-    game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(State)
-        if State == Enum.TeleportState.Started then
-            queueteleport('if betterisfile("CometV2/Loader.lua") then loadstring(readfile("CometV2/Loader.lua"))() else loadstring(game:HttpGet("https://raw.githubusercontent.com/Ham-135/CometV2/main/Loader.lua"))()')
-        end
+task.wait(0.5)
+shared["CometConfigs"] = {
+    StrokeTransparency = 0.75,
+    Color = Color3.fromRGB(255,65,65),
+    Enabled = false
+}
+local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Ham-135/CometV2/main/GuiLibrary.lua"))()
+local getasset = getsynasset or getcustomasset
+local ScreenGuitwo = game:GetService("CoreGui").RektskyNotificationGui
+local lplr = game:GetService("Players").LocalPlayer
+local cam = game:GetService("Workspace").CurrentCamera
+function CreateNotification(title,text,delay2)
+    spawn(function()
+        if ScreenGuitwo:FindFirstChild("Background") then ScreenGuitwo:FindFirstChild("Background"):Destroy() end
+        local frame = Instance.new("Frame")
+        frame.Size = UDim2.new(0, 100, 0, 115)
+        frame.Position = UDim2.new(0.5, 0, 0, -115)
+        frame.BorderSizePixel = 0
+        frame.AnchorPoint = Vector2.new(0.5, 0)
+        frame.BackgroundTransparency = 0.5
+        frame.BackgroundColor3 = Color3.new(0, 0, 0)
+        frame.Name = "Background"
+        frame.Parent = ScreenGuitwo
+        local frameborder = Instance.new("Frame")
+        frameborder.Size = UDim2.new(1, 0, 0, 8)
+        frameborder.BorderSizePixel = 0
+        frameborder.BackgroundColor3 = Color3.fromRGB(205, 64, 78)
+        frameborder.Parent = frame
+        local frametitle = Instance.new("TextLabel")
+        frametitle.Font = Enum.Font.SourceSansLight
+        frametitle.BackgroundTransparency = 1
+        frametitle.Position = UDim2.new(0, 0, 0, 30)
+        frametitle.TextColor3 = Color3.fromRGB(205, 64, 78)
+        frametitle.Size = UDim2.new(1, 0, 0, 28)
+        frametitle.Text = "          "..title
+        frametitle.TextSize = 24
+        frametitle.TextXAlignment = Enum.TextXAlignment.Left
+        frametitle.TextYAlignment = Enum.TextYAlignment.Top
+        frametitle.Parent = frame
+        local frametext = Instance.new("TextLabel")
+        frametext.Font = Enum.Font.SourceSansLight
+        frametext.BackgroundTransparency = 1
+        frametext.Position = UDim2.new(0, 0, 0, 68)
+        frametext.TextColor3 = Color3.new(1, 1, 1)
+        frametext.Size = UDim2.new(1, 0, 0, 28)
+        frametext.Text = "          "..text
+        frametext.TextSize = 24
+        frametext.TextXAlignment = Enum.TextXAlignment.Left
+        frametext.TextYAlignment = Enum.TextYAlignment.Top
+        frametext.Parent = frame
+        local textsize = game:GetService("TextService"):GetTextSize(frametitle.Text, frametitle.TextSize, frametitle.Font, Vector2.new(100000, 100000))
+        local textsize2 = game:GetService("TextService"):GetTextSize(frametext.Text, frametext.TextSize, frametext.Font, Vector2.new(100000, 100000))
+        if textsize2.X > textsize.X then textsize = textsize2 end
+        frame.Size = UDim2.new(0, textsize.X + 38, 0, 115)
+        pcall(function()
+            frame:TweenPosition(UDim2.new(0.5, 0, 0, 20), Enum.EasingDirection.InOut, Enum.EasingStyle.Quad, 0.15)
+            game:GetService("Debris"):AddItem(frame, delay2 + 0.15)
+        end)
     end)
-end)
-local placeid = game.PlaceId
-if placeid == 6872274481 or placeid == 8560631822 or placeid == 8444591321 then
-    if betterisfile("CometV2/Modules/Bedwars.lua") then
-        loadstring(readfile("CometV2/Modules/Bedwars.lua"))()
-    else
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Ham-135/CometV2/main/Modules/Bedwars.lua"))()
-    end
-elseif placeid == 6872265039 then
-    if betterisfile("CometV2/Modules/6872265039.lua") then
-        loadstring(readfile("CometV2/Modules/6872265039.lua"))()
-    else
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Ham-135/CometV2/main/Modules/6872265039.lua"))()
-    end
-else
-    if betterisfile("CometV2/Modules/Universal.lua") then
-        loadstring(readfile("CometV2/Modules/Universal.lua"))()
-    else
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Ham-135/CometV2/main/Modules/Universal.lua"))()
-    end
 end
+function runcode(func)
+    func()
+end
+lib:CreateWindow()
+local Tabs = {
+    ["Combat"] = lib:CreateTab("Combat",Color3.fromRGB(252,80,68),"combat"),
+    ["Blatant"] = lib:CreateTab("Blatant",Color3.fromRGB(255,148,36),"movement"),
+    ["Render"] = lib:CreateTab("Render",Color3.fromRGB(59,170,222),"render"),
+    ["Utility"] = lib:CreateTab("Utility",Color3.fromRGB(83,214,110),"player"),
+    ["World"] = lib:CreateTab("World",Color3.fromRGB(52,28,228),"world"),
+    ["Exploits"] = lib:CreateTab("Exploits",Color3.fromRGB(157,39,41),"exploit")
+}
+
+runcode(function()
+    local clone
+    local char
+    local Enabled = false
+    local Disabler = Tabs["Exploits"]:CreateToggle({
+        ["Name"] = "Disabler",
+        ["Callback"] = function(Callback)
+            Enabled = Callback
+            if Enabled then
+                char = lplr.Character
+                char.Archivable = true
+                clone = char:Clone()
+                clone.Parent = game:GetService("Workspace")
+                cam.CameraSubject = clone:FindFirstChild("Humanoid")
+                char.Parent = nil
+                lplr.Character = clone
+                clone:FindFirstChild("Animate").Disabled = true
+                clone:FindFirstChild("Animate").Disabled = false
+                clone:FindFirstChild("HumanoidRootPart").Anchored = false
+            else
+                clone:Destroy()
+                char.Parent = game:GetService("Workspace")
+                lplr.Character = char
+                cam.CameraSubject = char:FindFirstChild("Humanoid")
+                char:FindFirstChild("Animate").Disabled = true
+                char:FindFirstChild("Animate").Disabled = false
+            end
+        end
+    })
+end)
+
+runcode(function()
+    local Enabled = false
+    local Disabler = Tabs["Exploits"]:CreateToggle({
+        ["Name"] = "DeathDisabler",
+        ["Callback"] = function(Callback)
+            Enabled = Callback
+            if Enabled then
+                lib["ToggleFuncs"]["DeathDisabler"](true)
+                local hum = lplr.Character:FindFirstChild("Humanoid")
+                hum:SetStateEnabled(Enum.HumanoidStateType.Dead,true)
+                hum:ChangeState(Enum.HumanoidRootPart.Dead)
+                repeat task.wait() until hum.MoveDirection ~= Vector3.zero
+                task.wait(0.2)
+                hum:SetStateEnabled(Enum.HumanoidStateType.Dead,false)
+                hum:ChangeState(Enum.HumanoidRootPart.Running)
+                CreateNotification("DeathDisabler","Anticheat Disabled!",5)
+            end
+        end
+    })
+end)
+
+runcode(function()
+    local function CapeFunction(char,texture)
+        local hum = char:WaitForChild("Humanoid")
+        local torso = nil
+        if hum.RigType == Enum.HumanoidRigType.R15 then
+            torso = char:WaitForChild("UpperTorso")
+        else
+            torso = char:WaitForChild("Torso")
+        end
+        local p = Instance.new("Part",torso.Parent)
+        p.Name = "Cape"
+        p.Anchored = false
+        p.CanCollide = false
+        p.TopSurface = 0
+        p.FormFactor = "Custom"
+        p.BottomSurface = 0
+        p.Size = Vector3.new(0.2,0.2,0.2)
+        p.Transparency = 1
+        local decal = Instance.new("Decal",p)
+        decal.Texture = texture
+        decal.Face = "Back"
+        local msh = Instance.new("BlockMesh",p)
+        msh.Scale = Vector3.new(9,17.5,0.5)
+        local motor = Instance.new("Motor",p)
+        motor.Part0 = p
+        motor.Part1 = torso
+        motor.MaxVelocity = 0.01
+        motor.C0 = CFrame.new(0,2,0) * CFrame.Angles(0,math.rad(90),0)
+        motor.C1 = CFrame.new(0,1,0.45) * CFrame.Angles(0,math.rad(90),0)
+        local wave = false
+        repeat
+            task.wait(1/44)
+            decal.Transparency = torso.Transparency
+            local ang = 0.1
+            local oldmag = torso.Velocity.Magnitude
+            local mv = 0.002
+            if wave then
+                ang = ang + ((torso.Velocity.Magnitude/10) * 0.05) + 0.05
+                wave = false
+            else
+                wave = true
+            end
+            ang = ang + math.min(torso.Velocity.Magnitude/11,0.5)
+            motor.MaxVelocity = math.min((torso.Velocity.Magnitude/111), 0.04)
+            motor.DesiredAngle = -ang
+            if motor.CurrentAngle < -0.2 and motor.DesiredAngle > -0.2 then
+                motor.MaxVelocity = 0.04
+            end
+            repeat task.wait() until motor.CurrentAngle == motor.DesiredAngle or math.abs(torso.Velocity.Magnitude - oldmag) >= (torso.Velocity.Magnitude/10) + 1
+            if torso.Velocity.Magnitude < 0.1 then
+                task.wait(0.1)
+            end
+        until not p or p.Parent ~= torso.Parent
+    end
+    local Connection
+    local Enabled = false
+    local Cape = Tabs["Render"]:CreateToggle({
+        ["Name"] = "Cape",
+        ["Callback"] = function(Callback)
+            Enabled = Callback
+            if Enabled then
+                spawn(function()
+                    CapeFunction(lplr.Character,getasset("rektsky/assets/cape.png"))
+                end)
+                Connection = lplr.CharacterAdded:Connect(function(v)
+                    task.wait(1.5)
+                    spawn(function()
+                        CapeFunction(lplr.Character,getasset("rektsky/assets/cape.png"))
+                    end)
+                end)
+            else
+                Connection:Disconnect()
+                if lplr.Character:FindFirstChild("Cape") then
+                    lplr.Character:FindFirstChild("Cape"):Destroy()
+                end
+            end
+        end
+    })
+end)
+
+task.delay(0.5, function()
+    CreateNotification("Comet V2","Comet V2 Loaded! Hope you enjoy! discord.gg/fJNbvucPS5",15)
+    game:GetService("StarterGui"):SetCore("ChatMakeSystemMessage",{
+        Text = "Comet V2 Loaded!\nMade by Ham135 and Car (Also on YT)!\ndiscord.gg/fJNbvucPS5",
+        Color = Color3.fromRGB(255,65,65),
+        Font = Enum.Font.SourceSansBold
+    })
+end)
