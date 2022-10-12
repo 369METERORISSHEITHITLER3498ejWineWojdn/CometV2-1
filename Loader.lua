f5ff07e26f7c70a9b2b693a36fcda4cd72587bef
Ham-135 <115642877+Ham-135@users.noreply.github.com>
2022-10-12 16:34:50 +0200

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -3,6 +3,14 @@ function betterisfile(path)
     local suc, res = pcall(function() readfile(path) end)
     return suc and res ~= nil
 end
+spawn(function()
+    local queueteleport = syn and syn.queue_on_teleport or queue_on_teleport or fluxus and fluxus.queue_on_teleport
+    game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(State)
+        if State == Enum.TeleportState.Started then
+            queueteleport('if betterisfile("CometV2/Loader.lua") then loadstring(readfile("CometV2/Loader.lua"))() else loadstring(game:HttpGet("https://raw.githubusercontent.com/Ham-135/CometV2/main/Loader.lua"))()')
+        end
+    end)
+end)
 local placeid = game.PlaceId
 if placeid == 6872274481 or placeid == 8560631822 or placeid == 8444591321 then
     if betterisfile("CometV2/Modules/Bedwars.lua") then
