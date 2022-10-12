b32ff24e00f60f757dd68d3664575a074147b5ca
Ham-135 <115642877+Ham-135@users.noreply.github.com>
2022-10-12 16:29:12 +0200

    Update 6872265039.lua

--- Modules/6872265039.lua
+++ Modules/6872265039.lua
@@ -1,52 +1,33 @@
-task.wait(0.5)
-shared["CometConfigs"] = {
-    StrokeTransparency = 0.75,
-    Color = Color3.fromRGB(255,65,65),
-    Enabled = false
-}
-local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Ham-135/CometV2/main/GuiLibrary.lua"))()
-local getasset = getsynasset or getcustomasset
-local ScreenGuitwo = game:GetService("CoreGui").RektskyNotificationGui
-local lplr = game:GetService("Players").LocalPlayer
-local cam = game:GetService("Workspace").CurrentCamera
-function runcode(func)
-    func()
+repeat task.wait() until game:IsLoaded()
+function betterisfile(path)
+    local suc, res = pcall(function() readfile(path) end)
+    return suc and res ~= nil
 end
-lib:CreateWindow()
-local Tabs = {
-    ["Combat"] = lib:CreateTab("Combat",Color3.fromRGB(252,80,68),"combat"),
-    ["Blatant"] = lib:CreateTab("Blatant",Color3.fromRGB(255,148,36),"movement"),
-    ["Render"] = lib:CreateTab("Render",Color3.fromRGB(59,170,222),"render"),
-    ["Utility"] = lib:CreateTab("Utility",Color3.fromRGB(83,214,110),"player"),
-    ["World"] = lib:CreateTab("World",Color3.fromRGB(52,28,228),"world"),
-    ["Exploits"] = lib:CreateTab("Exploits",Color3.fromRGB(157,39,41),"exploit")
-}
-
-runcode(function()
-    local clone
-    local char
-    local Enabled = false
-    local Disabler = Tabs["Exploits"]:CreateToggle({
-        ["Name"] = "Disabler",
-        ["Callback"] = function(Callback)
-            Enabled = Callback
-            if Enabled then
-                char = lplr.Character
-                char.Archivable = true
-                clone = char:Clone()
-                clone.Parent = game:GetService("Workspace")
-                cam.CameraSubject = clone:FindFirstChild("Humanoid")
-                char.Parent = nil
-                lplr.Character = clone
-                clone:FindFirstChild("Animate").Disabled = true
-                clone:FindFirstChild("Animate").Disabled = false
-                clone:FindFirstChild("HumanoidRootPart").Anchored = false
-            else
-                clone:Destroy()
-                char.Parent = game:GetService("Workspace")
-                lplr.Character = char
-                cam.CameraSubject = char:FindFirstChild("Humanoid")
-            end
+spawn(function()
+    local queueteleport = syn and syn.queue_on_teleport or queue_on_teleport or fluxus and fluxus.queue_on_teleport
+    game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(State)
+        if State == Enum.TeleportState.Started then
+            queueteleport('if betterisfile("CometV2/Loader.lua") then loadstring(readfile("CometV2/Loader.lua"))() else loadstring(game:HttpGet("https://raw.githubusercontent.com/Ham-135/CometV2/main/Loader.lua"))()')
         end
-    })
+    end)
 end)
+local placeid = game.PlaceId
+if placeid == 6872274481 or placeid == 8560631822 or placeid == 8444591321 then
+    if betterisfile("CometV2/Modules/Bedwars.lua") then
+        loadstring(readfile("CometV2/Modules/Bedwars.lua"))()
+    else
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Ham-135/CometV2/main/Modules/Bedwars.lua"))()
+    end
+elseif placeid == 6872265039 then
+    if betterisfile("CometV2/Modules/6872265039.lua") then
+        loadstring(readfile("CometV2/Modules/6872265039.lua"))()
+    else
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Ham-135/CometV2/main/Modules/6872265039.lua"))()
+    end
+else
+    if betterisfile("CometV2/Modules/Universal.lua") then
+        loadstring(readfile("CometV2/Modules/Universal.lua"))()
+    else
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Ham-135/CometV2/main/Modules/Universal.lua"))()
+    end
+end
