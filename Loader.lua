55465f3acb5412027c160de8355b987f33156fcf
Ham-135 <115642877+Ham-135@users.noreply.github.com>
2022-10-12 16:23:51 +0200

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,25 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Ham-135/CometV2/main/Modules/Bedwars.lua"))()
+repeat task.wait() until game:IsLoaded()
+function betterisfile(path)
+    local suc, res = pcall(function() readfile(path) end)
+    return suc and res ~= nil
+end
+local placeid = game.PlaceId
+if placeid == 6872274481 or placeid == 8560631822 or placeid == 8444591321 then
+    if betterisfile("CometV2/Modules/Bedwars.lua") then
+        loadstring(readfile("CometV2/Modules/Bedwars.lua"))()
+    else
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Ham-135/CometV2/main/Modules/Bedwars.lua"))()
+    end
+elseif placeid == 6872265039 then
+    if betterisfile("CometV2/Modules/6872265039.lua") then
+        loadstring(readfile("CometV2/Modules/6872265039.lua"))()
+    else
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Ham-135/CometV2/main/Modules/6872265039.lua"))()
+    end
+else
+    if betterisfile("CometV2/Modules/Universal.lua") then
+        loadstring(readfile("CometV2/Modules/Universal.lua"))()
+    else
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Ham-135/CometV2/main/Modules/Universal.lua"))()
+    end
+end
