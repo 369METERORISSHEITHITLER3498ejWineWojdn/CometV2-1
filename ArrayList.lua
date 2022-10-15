8b383bebd73d652d1c99ced06ff220b0e2fb5b85
Ham-135 <115642877+Ham-135@users.noreply.github.com>
2022-10-15 22:19:01 +0200

    Update ArrayList.lua

--- ArrayList.lua
+++ ArrayList.lua
@@ -1,38 +1,56 @@
-local ArrayList = {}
-local ArrayGui = Instance.new("ScreenGui",game:GetService("CoreGui"))
-local Frame = Instance.new("Frame",ArrayGui)
-local Layout = Instance.new("UIListLayout",Frame)
-ArrayGui.Enabled = shared["CometConfigs"].Enabled
-ArrayGui.ResetOnSpawn = false
-Frame.Size = UDim2.new(0.2,0,1,0)
-Frame.Position = UDim2.new(0.87,0,0,0)
-Frame.BackgroundTransparency = 1
-Frame.BorderSizePixel = 0
+local ArrayLib = {}
+local ui = Instance.new("ScreenGui")
+if syn then syn.protect_gui(ui) end
+ui.ResetOnSpawn = false
+ui.Enabled = shared["CometConfigs"].Enabled
+ui.Parent = game:GetService("CoreGui")
+local frm = Instance.new("Frame")
+frm.Size = UDim2.new(0.1,0,0.9451,0)
+frm.Position = UDim2.new(0.8998,0,0.0549,0)
+frm.BackgroundTransparency = 1
+frm.BorderSizePixel = 0
+frm.Parent = ui
+layout = Instance.new("UIListLayout",frm)
+local mark = Instance.new("TextLabel")
+mark.Position = UDim2.new(0.8998)
+mark.Size = UDim2.new(0,167,0,50)
+mark.BackgroundTransparency = 1
+mark.Text = "Comet V2"
+mark.TextColor3 = Color3.fromHSV(tick()%5/5,1,1)
+mark.TextScaled = true
+mark.Parent = ui
 
 spawn(function()
     repeat
         task.wait(1)
-        ArrayGui.Enabled = shared["CometConfigs"].Enabled
-    until not ArrayGui
+        ui.Enabled = shared["CometConfigs"].Enabled
+    until not ui
+end)
+spawn(function()
+    repeat
+        task.wait()
+        mark.TextColor3 = Color3.fromHSV(tick()%5/5,1,1)
+        mark.TextStrokeTransparency = shared["CometConfigs"].StrokeTransparency
+        mark.Visible = shared["CometConfigs"].Watermark
+    until not mark or not ui or not frm
 end)
 
-function ArrayList.Add(Name,Suffix)
+function ArrayLib.Add(Name,Suffix)
     local newname
     if Suffix then
-        newname = Name.." <font color='rgb(75,75,75)'>"..Suffix.."</font>"
+        newname = Name.." - "..Suffix
     else
         newname = Name
     end
     local label = Instance.new("TextLabel")
-    label.TextScaled = true
-    label.RichText = true
-    label.Size = UDim2.new(0,217,0,25)
-    label.BackgroundTransparency = 1
-    label.Text = newname
-    label.TextColor3 = Color3.fromHSV(tick()%5/5,1,1)
-    label.TextStrokeTransparency = shared["CometConfigs"].StrokeTransparency
     label.Name = Name
-    label.Parent = Frame
+    label.Text = newname
+    label.Size = UDim2.new(0,183,0,25)
+    label.BackgroundTransparency = 1
+    label.TextScaled = true
+    label.TextStrokeTransparency = shared["CometConfigs"].StrokeTransparency
+    label.TextColor3 = Color3.fromHSV(tick()%5/5,1,1)
+    label.Parent = frm
     spawn(function()
         repeat
             task.wait()
@@ -42,16 +60,12 @@ function ArrayList.Add(Name,Suffix)
     end)
 end
 
-function ArrayList.Remove(Name)
-    if Frame:FindFirstChild(Name) then
-        Frame:FindFirstChild(Name):Destroy()
+function ArrayLib.Remove(Name)
+    if frm:FindFirstChild(Name) then
+        frm:FindFirstChild(Name):Destroy()
     end
 end
 
-function ArrayList.SetDrag(value)
-    Frame.Draggable = value
-    Frame.Selectable = value
-    Frame.Active = value
-end
+function ArrayLib.SetDrag(val) end
 
-return ArrayList
+return ArrayLib
